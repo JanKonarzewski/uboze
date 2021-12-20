c33aeb59b742d523e19018b01f76e301db3bf975
Jan Konarzewski <jan.konarzewski@consultant.lego.com>
2021-12-20 16:03:49 +0100

    version 0.1 - change to daily static imige scope

--- app/src/main/java/pl/konarzewski/uboze/engine/Engine.kt
+++ app/src/main/java/pl/konarzewski/uboze/engine/Engine.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Environment
 import androidx.room.Room
 import org.joda.time.DateTime
+import org.joda.time.LocalDate
 import pl.konarzewski.uboze.database.dao.ImigeDao
 import pl.konarzewski.uboze.database.AppDatabase
 import pl.konarzewski.uboze.database.entity.Imige
@@ -13,7 +14,8 @@ class Engine(ctx: Context) {
 
     private var images: List<Imige>
     private val imigeDao: ImigeDao
-    private val currDate: DateTime = DateTime()
+    private val shift: Int = 6
+    private val shiftedCurrDate: LocalDate = DateTime().minusHours(shift).toLocalDate()
     private val db: AppDatabase
     val dateToRepeat = mapOf(
         1 to 1,
@@ -28,7 +30,7 @@ class Engine(ctx: Context) {
         db = Room.databaseBuilder(
             ctx,
             AppDatabase::class.java,
-            "uboze_app_database.db"
+            "uboze_application_database.db"
         ).allowMainThreadQueries().build()
 
         val path = Environment.getExternalStorageDirectory().toString() + "/DCIM/Screenshots"
@@ -46,8 +48,8 @@ class Engine(ctx: Context) {
                 internalImige.path.equals(databaseImige.path)
             } != null
         }
-        val imigesForToday = getImigesForToday(intersect)
-        val exceptSorted = except.sortedByDescending { it.lastModified() }
+        val imigesForToday = getImigesForToday(intersect).sortedByDescending { it.path }
+        val exceptSorted = except.sortedByDescending { it.path }
         val newFiles = exceptSorted.take(100 - imigesForToday.size)
         val newImiges = newFiles.map { file -> Imige(file.path, null, null) }
 
@@ -59,32 +61,40 @@ class Engine(ctx: Context) {
     }
 
     fun getImige(position: Int): Imige {
-        if (position > 1) {
-            val prevImige = images.get(position - 2)
-            if (isImigeForToday(prevImige))
-                updateImige(prevImige)
-        }
+        if (position > 1)
+            updateImige(position - 1)
+
         return images.get(position)
     }
 
-    fun updateImige(imige: Imige) {
+    fun updateImige(position: Int) {
+        val imige = images.get(position)
+
+        if (imige.last_rep_date == null ||
+            imige.rep_no == null ||
+            !imige.last_rep_date!!.minusHours(shift).toLocalDate().isEqual(shiftedCurrDate)
+        )
+            increment(imige)
+    }
+
+    private fun increment(imige: Imige) {
         imige.rep_no = imige.rep_no?.plus(1) ?: 1
         imige.last_rep_date = DateTime()
         imigeDao.insert(imige)
     }
 
     private fun getImigesForToday(imiges: List<Imige>): List<Imige> {
-
         return imiges.filter { imige ->
             isImigeForToday(imige)
         }
     }
 
     private fun isImigeForToday(imige: Imige): Boolean {
-        val shitedCurrDate = currDate.minusHours(6).toLocalDate()
-        if(imige.rep_no != null && imige.last_rep_date != null)
-            return !imige.last_rep_date!!.plusDays(dateToRepeat.get(imige.rep_no)!!).minusHours(6).toLocalDate()
-                .isAfter(shitedCurrDate)
+        val shiftedImigeDate = imige.last_rep_date!!.minusHours(shift).toLocalDate()
+
+        if (imige.rep_no != null && imige.last_rep_date != null)
+            return shiftedImigeDate.isEqual(shiftedCurrDate) ||
+                    !shiftedImigeDate.plusDays(dateToRepeat.get(imige.rep_no)!!).isAfter(shiftedCurrDate)
         else
             return true
     }
--- app/src/main/java/pl/konarzewski/uboze/ui/main/fragment/MainFragment.kt
+++ app/src/main/java/pl/konarzewski/uboze/ui/main/fragment/MainFragment.kt
@@ -1,10 +1,13 @@
 package pl.konarzewski.uboze.ui.main.fragment
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import org.joda.time.DateTime
 import pl.konarzewski.uboze.R
 import pl.konarzewski.uboze.engine.Engine
 
@@ -12,6 +15,8 @@ class MainFragment : Fragment() {
 
     private lateinit var viewPager: ViewPager2
     private lateinit var engine: Engine
+    private lateinit var sharedPref: SharedPreferences
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +27,9 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        sharedPref = activity?.getSharedPreferences(
+            getString(R.string.preference_file_key), Context.MODE_PRIVATE)!!
+
         engine = Engine(requireContext()!!.applicationContext)
         viewPager = view.findViewById(R.id.pager)
 
@@ -29,13 +37,22 @@ class MainFragment : Fragment() {
             override fun getItemCount(): Int = engine.size()
 
             override fun createFragment(position: Int): Fragment {
-
                 val fragment = WordItemFragment()
 
-                val i = engine.getImige(position).path
-                fragment.arguments = Bundle().apply { putString("dir", i) }
+                fragment.arguments = Bundle().apply { putString("dir", engine.getImige(position).path) }
                 return fragment
             }
+        }
+
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setState(position)
+            }
+        })
+
+        if (getStateTimestamp().minusHours(6).toLocalDate().isEqual(DateTime().minusHours(6).toLocalDate())) {
+            viewPager.setCurrentItem(getState())
         }
     }
 
@@ -45,5 +62,20 @@ class MainFragment : Fragment() {
                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         )
         super.onStart()
+    }
+
+    fun setState(page: Int) {
+        val editor = sharedPref!!.edit()
+        editor.putInt("page", page)
+        editor.putLong("timestamp_of_page", DateTime().millis)
+        editor.commit()
+    }
+
+    fun getState(): Int {
+        return sharedPref!!.getInt("page", 1)
+    }
+
+    fun getStateTimestamp(): DateTime {
+        return sharedPref!!.getLong("timestamp_of_page", 0L)?.let { DateTime(it) }
     }
 }

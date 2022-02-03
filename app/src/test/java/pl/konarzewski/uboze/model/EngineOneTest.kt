3a01f10998095e45217b2c03b2f64c2efdde6720
Jan Konarzewski <jan.konarzewski@consultant.lego.com>
2022-02-03 21:31:14 +0100

    unit test adjust

--- app/src/test/java/pl/konarzewski/uboze/model/EngineOneTest.kt
+++ app/src/test/java/pl/konarzewski/uboze/model/EngineOneTest.kt
@@ -10,29 +10,29 @@ class EngineOneTest {
     @Test
     fun getPathsToRepeat() {
         val input = listOf(
-            Image("test_path_01", 0, DateTime("2022-01-01T01:01:01.000"), null),
-            Image("test_path_02", 1, DateTime("2022-01-01T01:01:01.000"), null),
-            Image("test_path_03", 2, DateTime("2022-01-01T01:01:01.000"), null),
-            Image("test_path_04", 3, DateTime("2022-01-01T01:01:01.000"), null)
+            Image("test_path_01", 0, DateTime("2022-01-01T01:01:01.000"), 0L, true),
+            Image("test_path_02", 1, DateTime("2022-01-01T01:01:01.000"), 0L, true),
+            Image("test_path_03", 2, DateTime("2022-01-01T01:01:01.000"), 0L, true),
+            Image("test_path_04", 3, DateTime("2022-01-01T01:01:01.000"), 0L, true)
         )
 
         val outputDay01 = listOf(
-            Image("test_path_01", 0, DateTime("2022-01-01T01:01:01.000"), null),
+            Image("test_path_01", 0, DateTime("2022-01-01T01:01:01.000"), 0L, true),
         )
         getPathsToRepeatTestDay(input, outputDay01, DateTime("2022-01-01T01:01:01.000"))
 
         val outputDay02 = listOf(
-            Image("test_path_01", 0, DateTime("2022-01-01T01:01:01.000"), null),
-            Image("test_path_02", 1, DateTime("2022-01-01T01:01:01.000"), null),
-            Image("test_path_03", 2, DateTime("2022-01-01T01:01:01.000"), null)
+            Image("test_path_01", 0, DateTime("2022-01-01T01:01:01.000"), 0L, true),
+            Image("test_path_02", 1, DateTime("2022-01-01T01:01:01.000"), 0L, true),
+            Image("test_path_03", 2, DateTime("2022-01-01T01:01:01.000"), 0L, true)
             )
         getPathsToRepeatTestDay(input, outputDay02, DateTime("2022-01-02T01:01:01.000"))
 
         val outputDay03 = listOf(
-            Image("test_path_01", 0, DateTime("2022-01-01T01:01:01.000"), null),
-            Image("test_path_02", 1, DateTime("2022-01-01T01:01:01.000"), null),
-            Image("test_path_03", 2, DateTime("2022-01-01T01:01:01.000"), null),
-            Image("test_path_04", 3, DateTime("2022-01-01T01:01:01.000"), null)
+            Image("test_path_01", 0, DateTime("2022-01-01T01:01:01.000"), 0L, true),
+            Image("test_path_02", 1, DateTime("2022-01-01T01:01:01.000"), 0L, true),
+            Image("test_path_03", 2, DateTime("2022-01-01T01:01:01.000"), 0L, true),
+            Image("test_path_04", 3, DateTime("2022-01-01T01:01:01.000"), 0L, true)
         )
         getPathsToRepeatTestDay(input, outputDay03, DateTime("2022-01-03T01:01:01.000"))
     }
